ad0bd3e60516b97b7e185fec6ee2418a8446464e
Dinesh Tamang <dineshtamang7263@gmail.com>
2021-11-29 12:03:08 +0530

    add front-end

---
+++ src/components/create-exercise.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class CreateExercises extends Component {
+  render() {
+    return (
+      <div>
+        <p>your in CreateExercises</p>
+      </div>
+    );
+  }
+}
---
+++ src/components/create-user.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class CreateUser extends Component {
+  render() {
+    return (
+      <div>
+        <p>your in CreateUser</p>
+      </div>
+    );
+  }
+}
