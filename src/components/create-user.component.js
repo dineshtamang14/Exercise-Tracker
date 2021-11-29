c19725a78432dc35616ff158a2b34d2f652a0b2a
Dinesh Tamang <dineshtamang7263@gmail.com>
2021-11-29 14:04:06 +0530

    add user input

--- src/components/create-user.component.js
+++ src/components/create-user.component.js
@@ -1,10 +1,59 @@
 import React, { Component } from "react";
 
 export default class CreateUser extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onChangeUsername = this.onChangeUsername.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
+    this.state = {
+      username: "",
+    };
+  }
+
+  onChangeUsername(e) {
+    this.setState({
+      username: e.target.value,
+    });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const user = {
+      username: this.state.username,
+    };
+
+    console.log(user);
+
+    // redirect user to home page
+    // window.location = "/";`
+
+    this.setState({
+      username: "",
+    })
+  }
+
   render() {
     return (
       <div>
-        <p>your in CreateUser</p>
+        <h3>Create New User</h3>
+        <form>
+          <div className="form-group">
+            <label>Username: </label>
+            <input
+              type="text"
+              required
+              className="form-control"
+              value={this.state.username}
+              onChange={this.onChangeUsername}
+            />
+          </div>
+          <div className="form-group">
+            <input type="submit" className="btn btn-primary" value="Create User" />
+          </div>
+        </form>
       </div>
     );
   }
