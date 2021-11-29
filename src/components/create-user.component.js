2e661e678d4979a77cccb0162b322681cf94d1b0
Dinesh Tamang <dineshtamang7263@gmail.com>
2021-11-29 14:39:28 +0530

    add exercise list

--- src/components/create-user.component.js
+++ src/components/create-user.component.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 export default class CreateUser extends Component {
   constructor(props) {
@@ -28,7 +29,11 @@ export default class CreateUser extends Component {
     console.log(user);
 
     // redirect user to home page
-    // window.location = "/";`
+    // window.location = "/";
+
+    //sending data to backend
+    axios.post("http://localhost:5000/users/add", user)
+      .then(res => console.log(res.data))
 
     this.setState({
       username: "",
@@ -39,7 +44,7 @@ export default class CreateUser extends Component {
     return (
       <div>
         <h3>Create New User</h3>
-        <form>
+        <form onSubmit={this.onSubmit}>
           <div className="form-group">
             <label>Username: </label>
             <input
@@ -51,7 +56,11 @@ export default class CreateUser extends Component {
             />
           </div>
           <div className="form-group">
-            <input type="submit" className="btn btn-primary" value="Create User" />
+            <input
+              type="submit"
+              className="btn btn-primary"
+              value="Create User"
+            />
           </div>
         </form>
       </div>
--- src/components/exercises-list.component.js
+++ src/components/exercises-list.component.js
@@ -1,6 +1,35 @@
 import React, { Component } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 export default class ExercisesList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.deleteExercise = this.deleteExercise.bind(this);
+    this.state = {
+      exercises: [],
+    }
+  }
+
+  componentDidMount() {
+    axios.get("http://localhost:5000/exercises/")
+      .then(response => {
+        this.setState({
+          exercises: response.data,
+        })
+      })
+      .catch(err => console.log(err));
+  }
+
+  deleteExercise(id) {
+    axios.delete("http://localhost:5000/exercises/" + id)
+      .then(res => console.log(res.data));
+      this.setState({
+        exercises: this.state.exercises.filter(el => el.id !== id)
+      })
+  }
+
   render() {
     return (
       <div>
